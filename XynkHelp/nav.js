0b056155405e82c91ec339fad4408fb8b660de84
Chuck Houpt <chuck@habilis.net>
2015-03-10 23:25:06 +0000

    Improved help menu animation - Use data attribute to store each sub-list's height, rather then use arbitrary max-height - Directly toggle the sub-lists' max-height, rather then use a collapse style - Added animate class, so animation can be applied after DOM manipulation is finished

--- XynkHelp/nav.js
+++ XynkHelp/nav.js
@@ -1,11 +1,20 @@
-(function () {
-	var groups = document.querySelectorAll('.group');
-	for (var i=0; i < groups.length; i++) {
-		var anchor = groups[i];
-		var list = anchor.nextSibling;
-		if (! list.querySelector('.active')) list.classList.toggle('collapsed');
-		anchor.addEventListener('click', function() {
-			this.nextSibling.classList.toggle('collapsed');
-		});
+document.onreadystatechange = function () {
+	switch (document.readyState) {
+		case 'interactive':
+			var groups = document.querySelectorAll('.group');
+			for (var i= 0; i < groups.length; i++) {
+				var anchor = groups[i];
+				var list = anchor.nextSibling;
+				list.dataset.height = list.clientHeight + 'px';
+				list.style.maxHeight = list.querySelector('.active') ? list.dataset.height : '0px';
+				anchor.addEventListener('click', function() {
+					var list = this.nextSibling;
+					list.style.maxHeight = (list.style.maxHeight == '0px') ? list.dataset.height : '0px';
+				});
+			}
+			break;
+		case 'complete':
+			document.querySelector('header').classList.toggle('animate', true);
+			break;
 	}
-})();
+};
